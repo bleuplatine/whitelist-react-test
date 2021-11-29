e3f37a0a2b879a17a2730edb5ec51a4de9b01b48
Sebastien V <bleuplatine@gmail.com>
2021-11-29 12:13:59 +0100

    Updates

---
+++ precache-manifest.96cd7c72ac6e4d19de31116d3ad2d876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c68ccaec8d1a3d8a39b9bbcb6d70fba",
+    "url": "/whitelist-react-test/index.html"
+  },
+  {
+    "revision": "d2821f5b4b83c9c5afd5",
+    "url": "/whitelist-react-test/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "7db4c11a396d9c7afe92",
+    "url": "/whitelist-react-test/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "d2821f5b4b83c9c5afd5",
+    "url": "/whitelist-react-test/static/js/2.f9a95594.chunk.js"
+  },
+  {
+    "revision": "7db4c11a396d9c7afe92",
+    "url": "/whitelist-react-test/static/js/main.33f13d9e.chunk.js"
+  },
+  {
+    "revision": "bba1a8f9bc2b816af981",
+    "url": "/whitelist-react-test/static/js/runtime-main.2aa85b67.js"
+  }
+]);
